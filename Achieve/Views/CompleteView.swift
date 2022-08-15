ab76662358e82f4dcb9279b725252f15ad668424
alexstarosta <alexstarostadev@gmail.com>
2022-08-14 23:27:52 -0400

    added completed goal animations

--- Achieve/Views/CompleteView.swift
+++ Achieve/Views/CompleteView.swift
@@ -14,6 +14,14 @@ struct CompleteView: View {
     
     let goal: Goal
     let bottomTextType: Int
+    
+    @State var iconOpacity = 0
+    @State var titleOpacity = 0
+    @State var goalOpacity = 0
+    
+    @State var iconOffset = 50
+    @State var titleOffset = 50
+    @State var goalOffset = 50
 
     @Environment(\.presentationMode) var presentationMode
     
@@ -28,12 +36,30 @@ struct CompleteView: View {
                     .foregroundColor(Color(UIColor.systemBackground))
             }
             .padding(.top, 190)
+            .offset(y: CGFloat(iconOffset))
+            .opacity(Double(iconOpacity))
+            .onAppear() {
+                withAnimation(.spring(dampingFraction: 0.4).delay(0.2), {iconOffset = 0})
+                withAnimation(.spring().delay(0.2), {iconOpacity = 1})
+            }
             
             Text("Goal Complete!")
                 .multilineTextAlignment(.center)
                 .font(.largeTitle.bold())
+                .offset(y: CGFloat(titleOffset))
+                .opacity(Double(titleOpacity))
+                .onAppear() {
+                    withAnimation(.spring(dampingFraction: 0.4).delay(0.3), {titleOffset = 0})
+                    withAnimation(.linear(duration: 0.3).delay(0.3), {titleOpacity = 1})
+                }
             
             smallGoalView(goal: goal)
+                .offset(y: CGFloat(goalOffset))
+                .opacity(Double(goalOpacity))
+                .onAppear() {
+                    withAnimation(.spring(dampingFraction: 0.4).delay(0.4), {goalOffset = 0})
+                    withAnimation(.linear(duration: 0.5).delay(0.4), {goalOpacity = 1})
+                }
             
             Button(action: {
                 goal.state = .active
@@ -62,7 +88,12 @@ struct CompleteView: View {
 }
 
 struct CompleteView_Previews: PreviewProvider {
+    static let userData = UserData()
+    static let localUserData = LocalUserData()
     static var previews: some View {
         CompleteView(goal: Goal(), bottomTextType: 0)
+            .environmentObject(userData)
+            .environmentObject(localUserData)
     }
+        
 }
